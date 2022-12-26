39a58cd82ef6434974103167260e2785554a48a8
mshubitidze <59510085+mshubitidze@users.noreply.github.com>
2022-12-26 10:56:26 +0400

    lsp stuff

--- after/plugin/null-ls.lua
+++ after/plugin/null-ls.lua
@@ -2,14 +2,11 @@ local null_ls = require("null-ls")
 
 null_ls.setup({
     sources = {
-        -- lua
-        null_ls.builtins.formatting.stylua,
-
+        -- null_ls.builtins.diagnostic.stylua,
         -- python
         null_ls.builtins.formatting.black,
 
         -- web dev
-        null_ls.builtins.diagnostics.eslint_d,
         null_ls.builtins.formatting.prettierd,
         null_ls.builtins.code_actions.xo,
     },
--- lua/M/packer.lua
+++ lua/M/packer.lua
@@ -1,99 +1,99 @@
 vim.cmd([[packadd packer.nvim]])
 
 return require("packer").startup(function(use)
-	-- Packer can manage itself
-	use("wbthomason/packer.nvim")
+    -- Packer can manage itself
+    use("wbthomason/packer.nvim")
 
-	use({
-		"nvim-telescope/telescope.nvim",
-		tag = "0.1.0",
-		-- or          find_files                  , branch = '0.1.x',
-		requires = { { "nvim-lua/plenary.nvim" } },
-	})
+    use({
+        "nvim-telescope/telescope.nvim",
+        tag = "0.1.0",
+        -- or          find_files                  , branch = '0.1.x',
+        requires = { { "nvim-lua/plenary.nvim" } },
+    })
 
-	use("nvim-treesitter/nvim-treesitter", { run = ":TSUpdate" })
+    use("nvim-treesitter/nvim-treesitter", { run = ":TSUpdate" })
 
-	use("nvim-treesitter/playground")
+    use("nvim-treesitter/playground")
 
-	use("theprimeagen/harpoon")
+    use("theprimeagen/harpoon")
 
-	use("mbbill/undotree")
+    use("mbbill/undotree")
 
-	use("tpope/vim-fugitive")
+    use("tpope/vim-fugitive")
 
-	use({
-		"VonHeikemen/lsp-zero.nvim",
-		requires = {
-			-- LSP Support
-			{ "neovim/nvim-lspconfig" },
-			{ "williamboman/mason.nvim" },
-			{ "williamboman/mason-lspconfig.nvim" },
+    use({
+        "VonHeikemen/lsp-zero.nvim",
+        requires = {
+            -- LSP Support
+            { "neovim/nvim-lspconfig" },
+            { "williamboman/mason.nvim" },
+            { "williamboman/mason-lspconfig.nvim" },
 
-			-- Autocompletion
-			{ "hrsh7th/nvim-cmp" },
-			{ "hrsh7th/cmp-buffer" },
-			{ "hrsh7th/cmp-path" },
-			{ "saadparwaiz1/cmp_luasnip" },
-			{ "hrsh7th/cmp-nvim-lsp" },
-			{ "hrsh7th/cmp-nvim-lua" },
+            -- Autocompletion
+            { "hrsh7th/nvim-cmp" },
+            { "hrsh7th/cmp-buffer" },
+            { "hrsh7th/cmp-path" },
+            { "saadparwaiz1/cmp_luasnip" },
+            { "hrsh7th/cmp-nvim-lsp" },
+            { "hrsh7th/cmp-nvim-lua" },
 
-			-- Snippets
-			{ "L3MON4D3/LuaSnip" },
-			{ "rafamadriz/friendly-snippets" },
-		},
-	})
+            -- Snippets
+            { "L3MON4D3/LuaSnip" },
+            { "rafamadriz/friendly-snippets" },
+        },
+    })
 
-	use("jose-elias-alvarez/null-ls.nvim")
+    use("jose-elias-alvarez/null-ls.nvim")
 
-	use("folke/zen-mode.nvim")
+    use("folke/zen-mode.nvim")
 
-	use("github/copilot.vim")
+    use("github/copilot.vim")
 
-	use({
-		"nvim-lualine/lualine.nvim",
-		requires = { "kyazdani42/nvim-web-devicons", opt = true },
-	})
+    use({
+        "nvim-lualine/lualine.nvim",
+        requires = { "kyazdani42/nvim-web-devicons", opt = true },
+    })
 
-	use("brenoprata10/nvim-highlight-colors")
+    use("brenoprata10/nvim-highlight-colors")
 
-	use("JoosepAlviste/nvim-ts-context-commentstring")
+    use("JoosepAlviste/nvim-ts-context-commentstring")
 
-	use("tpope/vim-commentary")
+    use("tpope/vim-commentary")
 
-	use("kylechui/nvim-surround")
+    use("kylechui/nvim-surround")
 
-	use({
-		"rose-pine/neovim",
-		as = "rose-pine",
-		config = function()
-			vim.cmd("colorscheme rose-pine")
-		end,
-	})
+    use({
+        "rose-pine/neovim",
+        as = "rose-pine",
+        config = function()
+            vim.cmd("colorscheme rose-pine")
+        end,
+    })
 
-	use("EdenEast/nightfox.nvim")
+    use("EdenEast/nightfox.nvim")
 
-	use("ellisonleao/gruvbox.nvim")
+    use("ellisonleao/gruvbox.nvim")
 
-	use("bluz71/vim-nightfly-colors")
+    use("bluz71/vim-nightfly-colors")
 
-	use("folke/tokyonight.nvim")
+    use("folke/tokyonight.nvim")
 
-	use({ "catppuccin/nvim", as = "catppuccin" })
+    use({ "catppuccin/nvim", as = "catppuccin" })
 
-	use({
-		"folke/trouble.nvim",
-		requires = "kyazdani42/nvim-web-devicons",
-		config = function()
-			require("trouble").setup({
-				-- your configuration comes here
-				-- or leave it empty to use the default settings
-				-- refer to the configuration section below
-			})
-		end,
-	})
+    use({
+        "folke/trouble.nvim",
+        requires = "kyazdani42/nvim-web-devicons",
+        config = function()
+            require("trouble").setup({
+                -- your configuration comes here
+                -- or leave it empty to use the default settings
+                -- refer to the configuration section below
+            })
+        end,
+    })
 
-	use({
-		"lewis6991/gitsigns.nvim",
-		-- tag = 'release' -- To use the latest release (do not use this if you run Neovim nightly or dev builds!)
-	})
+    use({
+        "lewis6991/gitsigns.nvim",
+        -- tag = 'release' -- To use the latest release (do not use this if you run Neovim nightly or dev builds!)
+    })
 end)
