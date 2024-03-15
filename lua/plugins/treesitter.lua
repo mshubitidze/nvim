3d229906e858ae8452460375378755ba00d5b065
mshubitidze <59510085+mshubitidze@users.noreply.github.com>
2024-03-15 23:20:07 +0400

    rm unused init.lua, rm tmux from ensure installed

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,7 +4,7 @@ return {
   config = function()
     ---@diagnostic disable-next-line: missing-fields
     require('nvim-treesitter.configs').setup {
-      ensure_installed = { 'bash', 'html', 'lua', 'markdown', 'vim', 'vimdoc', 'javascript', 'typescript', 'tsx', 'tmux', 'css' },
+      ensure_installed = { 'bash', 'html', 'lua', 'markdown', 'vim', 'vimdoc', 'javascript', 'typescript', 'tsx', 'css' },
       auto_install = true,
       highlight = { enable = true },
       indent = { enable = true },
