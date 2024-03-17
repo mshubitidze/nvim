b1d3a92b860e7ab1b619c0be3bbf9f0dc0917d55
mshubitidze <59510085+mshubitidze@users.noreply.github.com>
2024-03-17 22:43:52 +0400

    add neogit

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,37 +1,61 @@
--- {
---   'EdenEast/nightfox.nvim',
---   lazy = false,
---   priority = 1000,
---   config = function()
---     vim.cmd.colorscheme 'carbonfox'
---
---     vim.cmd.hi 'Comment gui=none'
---   end,
--- },
-
 return {
-  'catppuccin/nvim',
-  lazy = false,
-  name = 'catppuccin',
-  priority = 1000,
-  config = function()
-    require('catppuccin').setup {
-      flavour = 'mocha',
-      color_overrides = {
-        mocha = {
-          base = '#000000',
-          mantle = '#000000',
-          crust = '#000000',
+  -- {
+  --   'catppuccin/nvim',
+  --   lazy = false,
+  --   name = 'catppuccin',
+  --   priority = 1000,
+  --   config = function()
+  --     require('catppuccin').setup {
+  --       flavour = 'mocha',
+  --       color_overrides = {
+  --         mocha = {
+  --           base = '#000000',
+  --           mantle = '#000000',
+  --           crust = '#000000',
+  --         },
+  --       },
+  --       integrations = {
+  --         mini = {
+  --           enabled = true,
+  --         },
+  --       },
+  --     }
+  --     vim.cmd.colorscheme 'catppuccin-mocha'
+  --
+  --     vim.cmd.hi 'Comment gui=none'
+  --   end,
+  -- },
+  {
+    'EdenEast/nightfox.nvim',
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require('nightfox').setup {
+        palettes = {
+          duskfox = {
+            bg1 = '#000000',
+            bg0 = '#090909',
+            bg3 = '#121820',
+            sel0 = '#131b24',
+          },
         },
-      },
-      integrations = {
-        mini = {
-          enabled = true,
+        specs = {
+          all = {
+            inactive = 'bg0',
+          },
+          duskfox = {
+            inactive = '#090909',
+          },
         },
-      },
-    }
-    vim.cmd.colorscheme 'catppuccin-mocha'
+        groups = {
+          all = {
+            NormalNC = { fg = 'fg1', bg = 'inactive' },
+          },
+        },
+      }
+      vim.cmd.colorscheme 'duskfox'
 
-    vim.cmd.hi 'Comment gui=none'
-  end,
+      vim.cmd.hi 'Comment gui=none'
+    end,
+  },
 }
